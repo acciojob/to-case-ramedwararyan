0a4a458dd59c0d340dfb682259c334ec627d2b9c
ramedwararyan <143419537+ramedwararyan@users.noreply.github.com>
2025-08-02 09:03:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 function toCase(text) {
   // write your code here
+	if (text.length === 0) return "-";
+  
+  // Convert to lowercase and uppercase and join with '-'
+  return text.toLowerCase() + "-" + text.toUpperCase();
+	
 }
 
 // DO not change the code below
